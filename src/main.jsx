324c3b4a37bc267307b4be48c2d3511a45440e9b
Mohd-Feroz-8624 <mohamedferoz2023.com>
2025-06-05 00:04:54 +0530

    fixed route issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-  <BrowserRouter basename='/Sticky-Notes'>
-=======
-  <BrowserRouter basename="/Sticky-Notes">
->>>>>>> f9cdb67022a1ebcade051ccea7e2a852cf8cd40e
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
