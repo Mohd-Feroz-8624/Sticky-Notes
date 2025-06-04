e2ffaaa146c8fae0276b2512d9aee86bf41196a1
Mohd-Feroz-8624 <mohamedferoz2023@gmail.com>
2025-06-04 23:11:25 +0530

    4

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter baseName="/Sticky-Notes">
+  <BrowserRouter basename="/Sticky-Notes">
     <App />
   </BrowserRouter>
 )
